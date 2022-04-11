6dc076c1b90c1191063061ed76dc760047fbad9e
berkayyoner <berkayonerr2002@gmail.com>
2022-04-11 13:14:48 +0300

    v1.1.3

--- BitirmeProjesi/Formlar/GirisEkrani.cs
+++ BitirmeProjesi/Formlar/GirisEkrani.cs
@@ -22,6 +22,7 @@ namespace BitirmeProjesi
         {
             lblHataMesaji.Text = "";
             #region Merkeze Konumlandırma
+            this.Anchor = AnchorStyles.None;
             int x = (Screen.PrimaryScreen.WorkingArea.Width - this.Width) / 5;
             int y = (Screen.PrimaryScreen.WorkingArea.Height - this.Height) / 3;
             this.Location = new Point(x, y);
---
+++ BitirmeProjesi/Formlar/KayitEkrani.Designer.cs
@@ -0,0 +1,79 @@
+﻿namespace BitirmeProjesi.Formlar
+{
+    partial class KayitEkrani
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(243, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 37);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Kayıt Ol";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(197, 65);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 19);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Yıldızlı (*) alanlar boş bırakılamaz";
+            // 
+            // KayitEkrani
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(30)))), ((int)(((byte)(30)))), ((int)(((byte)(30)))));
+            this.ClientSize = new System.Drawing.Size(637, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "KayitEkrani";
+            this.Text = "Gütüphane";
+            this.Load += new System.EventHandler(this.KayitEkrani_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
