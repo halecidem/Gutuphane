36844d64ee4222113e270994d4f99ccef7b65602
berkayyoner <berkayonerr2002@gmail.com>
2022-05-28 08:43:28 +0300

    v1.2.24

--- BitirmeProjesi/Formlar/GitapYaz.cs
+++ BitirmeProjesi/Formlar/GitapYaz.cs
@@ -23,19 +23,20 @@ namespace BitirmeProjesi
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            #region NavBar'a Yanaştırma
-            NavBar navBar = new NavBar(kullaniciAdi);
-            this.Anchor = AnchorStyles.Left | AnchorStyles.Top;
-            this.Location = new Point(navBar.Size.Width, this.Location.Y);
-            this.Size = new Size(this.MdiParent.Size.Width - navBar.Size.Width - 20, this.MdiParent.Size.Height - 45);
-            #endregion
             #region Otomatik Boyutlandırma
+            NavBar navBar = new NavBar(kullaniciAdi);
             this.Size = new Size(this.MdiParent.Size.Width - navBar.Size.Width - 20, this.MdiParent.Size.Height - 45);
             #endregion
         }
 
         private void KitapYaz_Load(object sender, EventArgs e)
         {
+            #region NavBar'a Yanaştırma
+            NavBar navBar = new NavBar(kullaniciAdi);
+            this.Anchor = AnchorStyles.Left | AnchorStyles.Top;
+            this.Location = new Point(navBar.Size.Width, this.Location.Y);
+            this.Size = new Size(this.MdiParent.Size.Width - navBar.Size.Width - 20, this.MdiParent.Size.Height - 45);
+            #endregion
             timer1.Enabled = true;
         }
 
--- BitirmeProjesi/Formlar/Profil.cs
+++ BitirmeProjesi/Formlar/Profil.cs
@@ -22,6 +22,12 @@ namespace BitirmeProjesi
 
         private void Profil_Load(object sender, EventArgs e)
         {
+            #region NavBar'a Yanaştırma
+            NavBar navBar = new NavBar(kullaniciAdi);
+            this.Anchor = AnchorStyles.Left | AnchorStyles.Top;
+            this.Location = new Point(navBar.Size.Width, this.Location.Y);
+            this.Size = new Size(this.MdiParent.Size.Width - navBar.Size.Width - 20, this.MdiParent.Size.Height - 45);
+            #endregion
             timer1.Enabled = true;
             GenelIslemler gi = new GenelIslemler();
             gi.ProfilBilgileri(kullaniciAdi, lblAdi, lblSoyadi, lblEposta, lblKayitTarihi);
@@ -32,13 +38,8 @@ namespace BitirmeProjesi
 
         private void timer1_Tick(object sender, EventArgs e)
         {
-            #region NavBar'a Yanaştırma
-            NavBar navBar = new NavBar(kullaniciAdi);
-            this.Anchor = AnchorStyles.Left | AnchorStyles.Top;
-            this.Location = new Point(navBar.Size.Width, this.Location.Y);
-            this.Size = new Size(this.MdiParent.Size.Width - navBar.Size.Width - 20, this.MdiParent.Size.Height - 45);
-            #endregion
             #region Otomatik Boyutlandırma
+            NavBar navBar = new NavBar(kullaniciAdi);
             this.Size = new Size(this.MdiParent.Size.Width - navBar.Size.Width - 20, this.MdiParent.Size.Height - 45);
             #endregion
         }
